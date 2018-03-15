283c13e536a6ff660ef97b9ef6df53b072af708b
ximias <alexx4387@gmail.com>
2018-03-15 14:16:07 +0100

    GameState implementation added

--- src/main/java/common/data/GameState.java
+++ src/main/java/common/data/GameState.java
@@ -1,15 +1,68 @@
 package common.data;
 
+import common.services.Collidable;
+import common.services.Drawable;
+
 import java.util.ArrayList;
 
 public class GameState {
 	//Data:
 	ArrayList<Entity> gameEntities;
 	// Map Todo
-	// Game bounds Todo
-	// Time passed between frames
 	
-	//Functionality:
-	// Get all entities Todo
-	// Get all entities implementing a specific interface. Todo
+	public GameState() {
+		gameEntities = new ArrayList<Entity>(120);
+	}
+	
+	private int width, height;
+	private float deltaTime;
+	
+	public float getDeltaTime() {
+		return deltaTime;
+	}
+	
+	public void setDeltaTime(float deltaTime) {
+		this.deltaTime = deltaTime;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	public ArrayList<Entity> getGameEntities(){
+		return gameEntities;
+	}
+	
+	public void addEntity(Entity entity){
+		gameEntities.add(entity);
+	}
+	
+	/**
+	 * Get all entities implementing a specific interface
+	 * @param implementedInterface
+	 * @param <T>
+	 * @return
+	 */
+	public <T> ArrayList<T> getEntitiesByInterface(Class<T> implementedInterface){
+		// TODO test me!
+		ArrayList<T> list = new ArrayList<T>();
+		for (Entity gameEntity : gameEntities) {
+			if (implementedInterface.isAssignableFrom(gameEntity.getClass())){
+				list.add(implementedInterface.cast(gameEntity));
+			}
+		}
+		return list;
+	}
 }
